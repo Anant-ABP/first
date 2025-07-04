e40bc46e9a1527ee22030eb243d3ddb9df7b53bc
Panjari Anant <anantpanjari@gmail.com>
2025-07-04 13:25:12 +0530

    modified

---
+++ hello_world.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstc_
+{
+    internal class hello_world
+    {
+    }
+}
